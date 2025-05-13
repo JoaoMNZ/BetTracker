64d369cadef28292f72b010f2a2a73c7c126ac10
JoaoMNZ <joao.menezes21@Outlook.com>
2025-05-13 10:13:32 -0300

    Bet serialization / deserialization unit tests.

--- src/test/java/com/bet/tracker/service/BetJSONTests.java
+++ src/test/java/com/bet/tracker/service/BetJSONTests.java
@@ -23,7 +23,39 @@ public class BetJSONTests {
         assertThat(serializedBet).isStrictlyEqualToJson("Bet.json");
         assertThat(serializedBet).hasJsonPathNumberValue("@.id");
         assertThat(serializedBet).extractingJsonPathNumberValue("@.id").isEqualTo(1);
+        assertThat(serializedBet).hasJsonPathNumberValue("@.bettorID");
+        assertThat(serializedBet).extractingJsonPathNumberValue("@.bettorID").isEqualTo(1);
+        assertThat(serializedBet).hasJsonPathNumberValue("@.platformID");
+        assertThat(serializedBet).extractingJsonPathNumberValue("@.platformID").isEqualTo(1);
         assertThat(serializedBet).hasJsonPathStringValue("@.name");
-        assertThat(serializedBet).extractingJsonPathStringValue("@.name").isEqualTo("Bet365");
+        assertThat(serializedBet).extractingJsonPathStringValue("@.name").isEqualTo("Barcelona ML");
+        assertThat(serializedBet).hasJsonPathNumberValue("@.value");
+        assertThat(serializedBet).extractingJsonPathNumberValue("@.value").isEqualTo(5.00);
+        assertThat(serializedBet).hasJsonPathStringValue("@.date");
+        assertThat(serializedBet).extractingJsonPathStringValue("@.date").isEqualTo("2025-05-12");
+    }
+
+    @Test
+    public void BetDeserializationTest() throws IOException{
+        Bet bet = new Bet(1L,1L,1L,"Barcelona ML",5.00, LocalDate.of(2025,5,12));
+        String expected = """
+                {
+                  "id": 1,
+                  "bettorID": 1,
+                  "platformID": 1,
+                  "name": "Barcelona ML",
+                  "value": 5.00,
+                  "date": "2025-05-12"
+                }
+                """;
+        ObjectContent<Bet> deserializedBet = jacksonTester.parse(expected);
+        assertThat(deserializedBet).isEqualTo(bet);
+        assertThat(deserializedBet.getObject().id()).isEqualTo(bet.id());
+        assertThat(deserializedBet.getObject().bettorID()).isEqualTo(bet.bettorID());
+        assertThat(deserializedBet.getObject().platformID()).isEqualTo(bet.platformID());
+        assertThat(deserializedBet.getObject().name()).isEqualTo(bet.name());
+        assertThat(deserializedBet.getObject().value()).isEqualTo(bet.value());
+        assertThat(deserializedBet.getObject().date()).isEqualTo(bet.date());
+
     }
 }
